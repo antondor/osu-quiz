4fb75a2d951a1925d693121ede5a9c3d364a114a
AntonDor <maailo2401@gmail.com>
2024-02-08 23:12:23 +0200

    Minor header changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,4 @@
-import React from 'react'
 import styles from './header.module.css'
-import GitHub_Logo from '../../assets/images/GitHub_Logo.png'
-import OsuLogo_White from '../../assets/images/OsuLogo_White.svg'
 import { Link } from 'react-router-dom'
 
 function Header() {
@@ -10,14 +7,6 @@ function Header() {
       <div className={`${styles.header} container`}>
         <div className={styles.title}>
           <Link to='/'>Maiev&aposs osu! Quiz</Link>
-        </div>
-        <div className={styles.socials}>
-          <a href='https://osu.ppy.sh/users/10183830' className={styles.logoLink}>
-            <img src={OsuLogo_White} alt='#' className={styles.logo} />
-          </a>
-          <a href='https://github.com/antondor'>
-            <img src={GitHub_Logo} alt='#' className={styles.logo} />
-          </a>
         </div>
       </div>
     </div>
